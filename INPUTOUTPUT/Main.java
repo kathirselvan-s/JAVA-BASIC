d1383921b7758c6b685c20e6331b9602a2be50e8
kathirselvan-s <skathirselvan12@gmail.com>
2025-03-14 23:18:27 +0530

    Java basic learning

---
+++ INPUTOUTPUT/Main.java
@@ -0,0 +1,57 @@
+package INPUTOUTPUT;
+import java.util.*; 
+
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Scanner new_data = new Scanner(System.in); // Create Scanner object
+//
+//        System.out.print("Enter your name: ");
+//        String name = new_data.nextLine(); // Takes a string input
+//
+//        System.out.println("Hello, " + name + "!");
+//
+//        new_data.close(); // Close the scanner
+//    }
+//}
+//
+
+//public class Main{
+//	public static void main(String... kathir) {
+//		Scanner details = new Scanner(System.in);
+//		System.out.println("Enter the name\nEnter the dob\nEnter the age\nEnter the phone_num\nenter the gender");
+//		String name = details.nextLine();
+//		String dob = details.nextLine();
+//		int age = details.nextInt();
+//		details.nextLine();
+//		String phone_num = details.nextLine();
+//		String gender = details.nextLine(); 
+//		System.out.println(name+" "+dob+" "+age+" "+phone_num+" "+gender);
+//		details.close();	 // not important for small program but when use this here then in long program this is realy important.
+//	}
+//}
+
+
+public class Main{
+	public static void main(String... kathir) {
+		Scanner d = new Scanner(System.in);
+		System.out.println("Enter the age: ");
+		int age = d.nextInt();
+		System.out.println("Enter your height: (in meters)");
+		double height = d.nextDouble();
+		System.out.println("are you a student (Ture/Flase):");
+		boolean yn = d.nextBoolean();
+		System.out.println("Age = "+age);
+		System.out.println("height = "+height);
+		System.out.println("Student = "+yn);
+		d.close();
+	}
+}
+
+
+
+
+
+
+
+
---
+++ helloworld/Main.java
@@ -0,0 +1,88 @@
+package helloworld;
+
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("kathir");
+//	}
+//}
+//
+//
+
+
+//public class Main{
+//	public static void main(String[] args) {
+//		System.out.println("Kathirselvan");
+//		System.out.print("This is java");
+//	}
+//}
+
+
+
+//public class Main{
+//	public static void main(String[] args) {
+//		int a = 5;
+//		float b = 5.6f;
+//		double c = 6.9;
+//		System.out.printf("thr a is %d and float is %f and the double is %fl",a,b,c);
+//	}
+//}
+
+
+//public class Main{
+//	public static void main(String[] args) {
+//		String name = "kathir";
+//		int score = 70;
+//		System.out.format("student name is %s,and his score is %d",name,score);
+//	}
+//}
+
+
+//public class Main{
+//	public static void main(String[] args) {
+//		System.out.println("hello\nworld");
+//		System.out.println("hello\tworld");
+//		System.out.println("kathir is \"kathir is very good boy\"");
+//		System.out.println("kathir: \\\\");
+//	}
+//}
+
+
+
+//public class Main{
+//	public static void main(String[] args) {
+//		String name = "kathir";
+//		int num = 6;
+//		System.out.println("name is "+ name +" and his roll num is "+ num);
+//	}
+//}
+
+
+//public class Main{
+//	public static void main(String[] args) {
+//		for(int i=1;i<=10;i++) {
+//			System.out.println("Count : "+i);
+//		}
+//	}
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
